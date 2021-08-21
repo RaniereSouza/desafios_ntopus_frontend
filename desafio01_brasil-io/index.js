55d810d65154ed4190276f4e05f3ac027d126991
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2021-08-21 18:44:32 -0300

    [Desafio 01] solução incorreta para pegar os dados da API: requisições consecutivas à API geram uma resposta de erro HTTP 429 (Too Many Requests) e um bloqueio de CORS pelo seu servidor

--- desafio01_brasil-io/index.js
+++ desafio01_brasil-io/index.js
@@ -34,16 +34,81 @@
     }
   }
 
-  const apiRequest = new XMLHttpRequest(),
-        baseUrl    = 'https://api.brasil.io/v1';
+  /*==============================================================
+    Esta versão está incorreta por vários motivos... A idéia era
+    iterar recursivamente os dados da API, sempre continuando a
+    fazer novas requisições enquanto ainda tivessem dados para
+    serem buscados (campo "next" na resposta da requisição). O
+    problema é que atingimos um limite permitido pelo servidor da
+    API, ele só nos deixa fazer uma quantidade arbitrária de
+    requisições antes de sermos respondidos com um erro 429 - Too
+    Many Requests (com um bloqueio de CORS pelo servidor). O que é
+    normal, já que a API deles passou por problemas de abuso de
+    consumo de dados por usuários da API.
+    Mesmo forçando um intervalo de 2 segundos entre uma requisição
+    e outra, as requisições são interrompidas com entre 5 a 20
+    requisições consecutivas. Lembrando que no ambiente usado até
+    aqui, não há nenhum tipo de servidor local, o arquivo
+    index.html é aberto pelo browser "as is", sem acessar o
+    localhost ou algo do tipo: Como vinha sendo desenvolvido até
+    agora, na intenção de fazer tudo na forma mais "pura"
+    possível, não foi usado nada de Node.js, nginx, XAMPP, IIS, ou
+    outro tipo de servidor local, e também não foi utilizado
+    nenhum pacote auxiliar. 
+  ==============================================================*/
+
+  const apiRequest           = new XMLHttpRequest(),
+        baseUrl              = 'https://api.brasil.io/v1',
+        wantedTimeRangeRegex = /^(2018-01-)+([\d]{2})+$/;
+  
+  let info1Val = 0;
+
+  function makeRequestToAPI(url) {
+    const timeoutRef = setTimeout(() => {
+      apiRequest.open('GET', url, true);
+      apiRequest.overrideMimeType('application/json');
+      apiRequest.setRequestHeader('Authorization', `Token ${tokens["brasil.io-token"]}`);
+      apiRequest.send(null);
+      clearTimeout(timeoutRef);
+    }, 2000);
+  }
 
   apiRequest.onreadystatechange = function() {
     if (apiRequest.readyState === 4) {
-      removeLoadingSpinner();
-
       if (apiRequest.status === 200) {
         const responseJSON = JSON.parse(apiRequest.response);
         console.log('apiRequest.response: ', responseJSON);
+
+        if (responseJSON) {
+          if (responseJSON.results                 &&
+             (Array.isArray(responseJSON.results))) {
+            
+            info1Val = responseJSON.results.reduce((acc, item) => {
+              if (item && item.data_de_publicacao                        &&
+                 (typeof item.data_de_publicacao === 'string')           &&
+                 (wantedTimeRangeRegex.test(item.data_de_publicacao))    &&
+                 (item.total_de_rendimentos)                             &&
+                 (typeof Number(item.total_de_rendimentos) === 'number')) {
+
+                return (acc + Number(item.total_de_rendimentos));
+              } 
+            }, info1Val);
+          }
+
+          if (!responseJSON.next                      ||
+             (typeof responseJSON.next !== 'string')  ||
+             (responseJSON.next.trim() === ''))        {
+            
+            removeLoadingSpinner();
+            
+            console.log('info1Val: ', info1Val);
+            // preencher os elementos DOM com os valores calculados
+          }
+          else {
+            // continuar fazendo requisições para a API para calcular os valores finais
+            makeRequestToAPI(responseJSON.next);
+          }
+        }
       }
       else if (apiRequest.status >= 400) {
         console.log('Erro na requisição HTTP: ', `${apiRequest.status} ${apiRequest.statusText}`);
@@ -52,8 +117,5 @@
   }
 
   addLoadingSpinner();
-  apiRequest.open('GET', `${baseUrl}/dataset/salarios-magistrados/contracheque/data/`, true);
-  apiRequest.overrideMimeType('application/json');
-  apiRequest.setRequestHeader('Authorization', `Token ${tokens["brasil.io-token"]}`);
-  apiRequest.send(null);
+  makeRequestToAPI(`${baseUrl}/dataset/salarios-magistrados/contracheque/data/`);
 })();
