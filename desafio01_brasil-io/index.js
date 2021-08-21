dac08a6962a536d95f0cc8cacca4e8dafe732611
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2021-08-21 12:44:46 -0300

    [Desafio 01] testando a existência do XMLHttpRequest na execução atual, e testando a conexão com a API usando o token (no momento funcionando)

--- desafio01_brasil-io/index.js
+++ desafio01_brasil-io/index.js
@@ -7,6 +7,27 @@
     console.log('Não foi encontrado um token para a API do Brasil.io');
     return;
   }
+  if (!XMLHttpRequest) {
+    console.log('Este browser não suporta o XMLHttpRequest');
+    return;
+  }
 
-  console.log('Arquivo index.js rodando!');
+  const apiRequest = new XMLHttpRequest(),
+        baseUrl    = 'https://api.brasil.io/v1';
+
+  apiRequest.onreadystatechange = function() {
+    if (apiRequest.readyState === 4) {
+      if (apiRequest.status === 200) {
+        const response = apiRequest.response;
+        console.log('apiRequest.response: ', response);
+      }
+      else if (apiRequest.status >= 400) {
+        console.log('Erro na requisição HTTP: ', `${apiRequest.status} ${apiRequest.statusText}`);
+      }
+    }
+  }
+  apiRequest.open('GET', `${baseUrl}/`, true);
+  apiRequest.overrideMimeType('application/json');
+  apiRequest.setRequestHeader('Authorization', `Token ${tokens["brasil.io-token"]}`);
+  apiRequest.send(null);
 })();
