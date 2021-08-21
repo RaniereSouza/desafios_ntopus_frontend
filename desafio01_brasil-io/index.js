f2ebb96558d78f0dee06ce4804db526f3b5b7b0e
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2021-08-21 03:08:46 -0300

    [Desafio 01] adicionando arquivo tokens.js que vai armazenar o token (funcionando como um arquivo .env, por exemplo), e testando no index.js se o token para o Brasil.io existe no escopo global

--- desafio01_brasil-io/index.js
+++ desafio01_brasil-io/index.js
@@ -1,3 +1,12 @@
 (function() {
+  if (!tokens                                        ||
+     (!tokens['brasil.io-token'])                    ||
+     (typeof tokens['brasil.io-token'] !== 'string') ||
+     (tokens['brasil.io-token'].trim() === '')      ) {
+    
+    console.log('Não foi encontrado um token para a API do Brasil.io');
+    return;
+  }
+
   console.log('Arquivo index.js rodando!');
 })();
---
+++ desafio01_brasil-io/tokens.example.js
@@ -0,0 +1,3 @@
+var tokens = {
+  "brasil.io-token": "<Insira aqui seu token de acesso do Brasil.io>"
+};
