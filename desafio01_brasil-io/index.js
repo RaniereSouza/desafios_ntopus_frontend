1bede5cb391e2ea6b90c74b5262857a5f516f683
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2021-08-21 15:32:20 -0300

    [Desafio 01] testando a API /dataset/salarios-magistrados/contracheque/data/; conversão da resposta da requisição HTTP para objeto JSON, para facilitar a manipulação; adicionando loading spinner para sinalizar a espera de algum processamento (no momento, ele sinaliza apenas quando a requisição HTTP foi concluída)

--- desafio01_brasil-io/index.js
+++ desafio01_brasil-io/index.js
@@ -2,7 +2,7 @@
   if (!tokens                                        ||
      (!tokens['brasil.io-token'])                    ||
      (typeof tokens['brasil.io-token'] !== 'string') ||
-     (tokens['brasil.io-token'].trim() === '')      ) {
+     (tokens['brasil.io-token'].trim() === ''))       {
     
     console.log('Não foi encontrado um token para a API do Brasil.io');
     return;
@@ -11,22 +11,48 @@
     console.log('Este browser não suporta o XMLHttpRequest');
     return;
   }
+  if (!JSON) {
+    console.log('Este browser não tem acesso ao objeto global JSON');
+    return;
+  }
+
+  function addLoadingSpinner() {
+    const spinner = document.getElementById('loading-spinner');
+
+    if (!spinner) {
+      const newSpinner = document.createElement('span');
+
+      newSpinner.setAttribute('id', 'loading-spinner');
+      document.body.appendChild(newSpinner);
+    }
+  }
+  function removeLoadingSpinner() {
+    const spinner = document.getElementById('loading-spinner');
+
+    if (spinner) {
+      spinner.parentNode.removeChild(spinner);
+    }
+  }
 
   const apiRequest = new XMLHttpRequest(),
         baseUrl    = 'https://api.brasil.io/v1';
 
   apiRequest.onreadystatechange = function() {
     if (apiRequest.readyState === 4) {
+      removeLoadingSpinner();
+
       if (apiRequest.status === 200) {
-        const response = apiRequest.response;
-        console.log('apiRequest.response: ', response);
+        const responseJSON = JSON.parse(apiRequest.response);
+        console.log('apiRequest.response: ', responseJSON);
       }
       else if (apiRequest.status >= 400) {
         console.log('Erro na requisição HTTP: ', `${apiRequest.status} ${apiRequest.statusText}`);
       }
     }
   }
-  apiRequest.open('GET', `${baseUrl}/`, true);
+
+  addLoadingSpinner();
+  apiRequest.open('GET', `${baseUrl}/dataset/salarios-magistrados/contracheque/data/`, true);
   apiRequest.overrideMimeType('application/json');
   apiRequest.setRequestHeader('Authorization', `Token ${tokens["brasil.io-token"]}`);
   apiRequest.send(null);
