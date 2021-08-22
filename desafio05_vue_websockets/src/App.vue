7582162a1f805c6b5c4c1122a8ba171d8269b443
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2021-08-22 05:55:52 -0300

    [Desafio 05] base da lista de objetos TAnimal na corrida, do display da pista e dos marcadores dos animais (renderização de lista de elementos no template)

--- desafio05_vue_websockets/src/App.vue
+++ desafio05_vue_websockets/src/App.vue
@@ -1,30 +1,61 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <header id="header">
+      <h1>Seleção Ntopus - Desafio 05</h1>
+      <h2>Vue.js e WebSockets</h2>
+    </header>
+    <main>
+      <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+      <Corrida />
+    </main>
   </div>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
-  import HelloWorld from './components/HelloWorld.vue';
+
+  import Corrida from './components/Corrida.vue';
 
   export default defineComponent({
     name: 'App',
     components: {
-      HelloWorld
+      Corrida
     }
   });
 </script>
 
 <style>
-  #app {
+  * {
+    margin:      0;
+    padding:     0;
+    box-sizing:  border-box;
     font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 0.5rem;
-    min-height: calc(100vh - 1rem);
   }
+
+  #app {
+    -webkit-font-smoothing:  antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align:              center;
+    color:                   #2c3e50;
+  }
+
+    #app #header {
+      display:         flex;
+      flex-direction:  column;
+      justify-content: center;
+      width:           100%;
+      height:          90px;
+      border-bottom:   1px solid #ddd;
+      padding:         0.5rem 1rem;
+      text-align:      left;
+    }
+
+    #app main {
+      position:        relative;
+      display:         flex;
+      flex-direction:  row;
+      justify-content: center;
+      width:           100vw;
+      height:          calc(100vh - 90px);
+    }
 </style>
---
+++ desafio05_vue_websockets/src/components/Corrida.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="corrida-container">
+    <ul>
+      <li v-for="(animal, index) in animalsList" v-bind:key="index">
+        <div v-bind:title="animal.nome" class="animal-pin">
+          <span class="animal-popup">{{animal.nome}}</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  import { TAnimal } from '../@types/TAnimal';
+
+  export default defineComponent({
+    name: 'Corrida',
+    data() {
+      return {
+        animalsList: [
+          {nome: "Cavalo",  distancia: 0},
+          {nome: "Jaguar",  distancia: 0},
+          {nome: "Canguru", distancia: 0},
+          {nome: "Lobo",    distancia: 0},
+        ] as TAnimal[],
+      };
+    },
+  });
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #corrida-container {
+    display:         flex;
+    align-items:     center;
+    justify-content: center;
+    width:           100%;
+  }
+
+  ul {
+    list-style-type:  none;
+    width:            100%;
+    padding:          0;
+    background-color: #333;
+  }
+
+  li {
+    display:     flex;
+    align-items: center;
+    width:       100%;
+    height:      3rem;
+    color:       #fff;
+    text-align:  left;
+  }
+
+    li + li {
+      border-top: 1px dashed #fff;
+    }
+
+    li .animal-pin {
+      position:         relative;
+      width:            2rem;
+      height:           2rem;
+      border-radius:    50%;
+      overflow:         hidden;
+      background-color: aqua;
+    }
+
+      li .animal-pin > .animal-popup {
+        position: absolute;
+        opacity:  0;
+      }
+</style>
