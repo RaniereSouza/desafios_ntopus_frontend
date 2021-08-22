0433a0d5a3d3e3d839edbf3a0478ef2129e20e3d
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2021-08-22 06:17:23 -0300

    [Desafio 05] simulação do tempo de resposta do back-end + spinner de espera (lifecycle hook e renderização condicional no template)

--- desafio05_vue_websockets/src/components/Corrida.vue
+++ desafio05_vue_websockets/src/components/Corrida.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="corrida-container">
-    <ul>
+    <ul v-if="animalsList && (animalsList.length > 0)">
       <li v-for="(animal, index) in animalsList" v-bind:key="index">
         <div v-bind:title="animal.nome" class="animal-pin">
           <span class="animal-popup">{{animal.nome}}</span>
         </div>
       </li>
     </ul>
+    <span v-else class="loading-spinner"></span>
   </div>
 </template>
 
@@ -19,13 +20,22 @@
     name: 'Corrida',
     data() {
       return {
-        animalsList: [
-          {nome: "Cavalo",  distancia: 0},
-          {nome: "Jaguar",  distancia: 0},
-          {nome: "Canguru", distancia: 0},
-          {nome: "Lobo",    distancia: 0},
-        ] as TAnimal[],
+        animalsList: [] as TAnimal[],
       };
+    },
+    mounted() {
+      if (this.animalsList && Array.isArray(this.animalsList)) {
+        const timeoutRef = setTimeout(() => {
+          this.animalsList.push(
+            {nome: "Cavalo",  distancia: 0},
+            {nome: "Jaguar",  distancia: 0},
+            {nome: "Canguru", distancia: 0},
+            {nome: "Lobo",    distancia: 0},
+          );
+
+          clearTimeout(timeoutRef);
+        }, 3000);
+      }
     },
   });
 </script>
@@ -72,4 +82,40 @@
         position: absolute;
         opacity:  0;
       }
+
+    @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  .loading-spinner {
+    position:         relative;
+    display:          block;
+    width:            2rem;
+    height:           2rem;
+    border-radius:    50%;
+    background-color: #fff;
+    background-clip:  padding-box;
+    border:           0.25rem solid transparent;
+  }
+
+    .loading-spinner::before {
+      position:                  absolute;
+      z-index:                   -1;
+      top:                       -0.25rem;
+      right:                     -0.25rem;
+      width:                     2rem;
+      height:                    2rem;
+      border-radius:             inherit;
+      content:                   '';
+      background-image:          linear-gradient(to top right, #888, #ddd, #fff);
+      animation-name:            spin;
+      animation-duration:        500ms;
+      animation-iteration-count: infinite;
+      animation-timing-function: linear;
+    }
 </style>
